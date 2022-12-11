e83334b14d5d188d645bf4e56a244d6d24df5c34
Nicolas KEITA <nicolas.keita@epitech.eu>
2022-12-11 17:07:45 +0100

    v1.1.0 working on getting started with test...

--- src/desktop/desktop.test.tsx
+++ src/desktop/desktop.test.tsx
@@ -2,28 +2,28 @@
     Path + Filename: src/desktop/desktop.test.tsx
 */
 
-import React from "react";
+import React from 'react'
 // import {Provider} from 'react-redux'
 // import MyApp from './MyApp'
-import { AppWindow } from "../AppWindow";
-import { kWindowNames } from "../consts";
+import {AppWindow} from '../AppWindow'
+import {kWindowNames} from '../consts'
 // import {store} from '@utils/store/store'
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom'
 
-const myWindow = new AppWindow(kWindowNames.desktop);
+const myWindow = new AppWindow(kWindowNames.desktop)
 
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "@utils/store/store";
-import MyApp from "./MyApp";
+import {render} from '@testing-library/react'
+import {Provider} from 'react-redux'
+import {store} from '@utils/store/store'
+import MyApp from './MyApp'
 
-test("renders learn react link", () => {
-  render(
-    // <div>hello world</div>
-    <Provider store={store}>
-      <MyApp my_window={myWindow} />
-    </Provider>
-  );
-  // const linkElement = screen.getByText(/hello world/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+test('renders learn react link', () => {
+	render(
+		// <div>hello world</div>
+		<Provider store={store}>
+			<MyApp my_window={myWindow} />
+		</Provider>
+	)
+	// const linkElement = screen.getByText(/hello world/i);
+	// expect(linkElement).toBeInTheDocument();
+})
--- src/desktop/utils/fetchLocalConfigJson/fetchChampionsFromConfigJson.ts
+++ src/desktop/utils/fetchLocalConfigJson/fetchChampionsFromConfigJson.ts
@@ -3,44 +3,44 @@
 */
 
 
-import { Champion } from "../../components/maincontent/settings/Champion";
+import {Champion} from '../../components/maincontent/settings/Champion'
 
 export async function fetchAllChampionsJson(): Promise<Champion[]> {
-  const url = "https://4nuo1ouibd.execute-api.eu-west-3.amazonaws.com/csw_api_proxy/allchamps";
-  let res;
-  try {
-    res = await fetch(url);
-  } catch (e) {
-    throw new Error("CSW_error: following call : fetch(" + url + " caught error;  " + res.statusText);
-  }
-  try {
-    const data = await res.json() as Promise<Champion[]>;
-    return (Object.values(data).map((elem: Champion) => {
-      return {
-        name: elem.name,
-        opScore_user: elem.opScore_CSW,
-        opScore_CSW: elem.opScore_CSW,
-        image: elem.image,
-        imageUrl: "",
-        role: elem.role
-      };
-    }));
-  } catch (e) {
-    throw new Error("CSW_error: following call : res.json() caught error; previously : fetch(" + url);
-  }
+	const url = 'https://4nuo1ouibd.execute-api.eu-west-3.amazonaws.com/csw_api_proxy/allchamps'
+	let res
+	try {
+		res = await fetch(url)
+	} catch (e) {
+		throw new Error('CSW_error: following call : fetch(' + url + ' caught error;  ' + res.statusText)
+	}
+	try {
+		const data = await res.json() as Promise<Champion[]>
+		return (Object.values(data).map((elem: Champion) => {
+			return {
+				name: elem.name,
+				opScore_user: elem.opScore_CSW,
+				opScore_CSW: elem.opScore_CSW,
+				image: elem.image,
+				imageUrl: '',
+				role: elem.role
+			}
+		}))
+	} catch (e) {
+		throw new Error('CSW_error: following call : res.json() caught error; previously : fetch(' + url)
+	}
 }
 
 export async function fetchCSWgameVersion(): Promise<string> {
-  const url = "https://4nuo1ouibd.execute-api.eu-west-3.amazonaws.com/csw_api_proxy/cswgameversion";
-  let res;
-  try {
-    res = await fetch(url);
-  } catch (e) {
-    throw new Error("CSW_error: following call : fetch(" + url + " caught error;  ");
-  }
-  try {
-    return await res.text();
-  } catch (e) {
-    throw new Error("CSW_error: following call : res.text() caught error; previously : fetch(" + url);
-  }
+	const url = 'https://4nuo1ouibd.execute-api.eu-west-3.amazonaws.com/csw_api_proxy/cswgameversion'
+	let res
+	try {
+		res = await fetch(url)
+	} catch (e) {
+		throw new Error('CSW_error: following call : fetch(' + url + ' caught error;  ')
+	}
+	try {
+		return await res.text()
+	} catch (e) {
+		throw new Error('CSW_error: following call : res.text() caught error; previously : fetch(' + url)
+	}
 }
