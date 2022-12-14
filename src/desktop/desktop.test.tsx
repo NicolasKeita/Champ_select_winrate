9ddf1723dbd4402038c0cf34bf9565888ee8c310
Nicolas KEITA <nicolas.keita@epitech.eu>
2022-12-14 11:39:27 +0100

    v1.1.2 add: my first test, should display winrate in header

--- src/desktop/desktop.test.tsx
+++ src/desktop/desktop.test.tsx
@@ -8,62 +8,65 @@ import {Provider} from 'react-redux'
 import MyApp from './MyApp'
 import {AppWindow} from '../AppWindow'
 import {kWindowNames} from '../consts'
-import '@testing-library/jest-dom'
-import {act, render, waitFor} from '@testing-library/react'
+import {act, render, waitFor, screen} from '@testing-library/react'
 import {ChakraProvider} from '@chakra-ui/react'
 import {store} from '@utils/store/store'
 import * as fetchChampionsFromConfigJson from "@utils/fetchLocalConfigJson/fetchChampionsFromConfigJson"
 import fetchMock from "jest-fetch-mock";
+import allChamps from './../../__tests__/allChamps.json'
+import '@testing-library/jest-dom'
 
 const myWindow = new AppWindow(kWindowNames.desktop)
 
-test('renders learn react link', async () => {
-	const mock = jest.spyOn(fetchChampionsFromConfigJson, "fetchCSWgameVersion").mockResolvedValue("12.23.0")
-	const mock2 = jest.spyOn(fetchChampionsFromConfigJson, "fetchAllChampionsJson").mockResolvedValue([])
-	fetchMock.enableMocks()
-	// @ts-ignore
-	fetch.mockResponseOnce(JSON.stringify({rates: {CAD: 1.42}}));
-	// global.overwolf = jest.fn(() => // TODO do that
-	global.overwolf = {
-		windows: {
+describe('basic', () => {
+	test('should display winrate in title', async () => {
+		const mock = jest.spyOn(fetchChampionsFromConfigJson, "fetchCSWgameVersion").mockResolvedValue("12.23.0")
+		const mock2 = jest.spyOn(fetchChampionsFromConfigJson, "fetchAllChampionsJson").mockResolvedValue([])
+		fetchMock.enableMocks()
+		// @ts-ignore
+		fetch.mockResponseOnce(JSON.stringify(allChamps));
+		global.overwolf = {
+			games: {
+				launchers: {
+					//@ts-ignore
+					events: {
+						getInfo: (numberr: any, callback: any) => {}
+					},
+					getRunningLaunchersInfo: () => {},
+					//@ts-ignore
+					onLaunched: {
+						addListener: () => {}
+					},
+					//@ts-ignore
+					onTerminated: {
+						addListener: () => {}
+					}
+				}
+			},
 			//@ts-ignore
-			onStateChanged: {
-				addListener: (callback) => {}
-			}
-		},
-		games: {
-			launchers: {
-				//@ts-ignore
-				events: {
-					getInfo: (numberr: any, callback: any) => {}
-				},
-				getRunningLaunchersInfo: () => {},
-				//@ts-ignore
-				onLaunched: {
-					addListener: () => {}
-				},
-				//@ts-ignore
-				onTerminated: {
-					addListener: () => {}
+			windows: {
+				onStateChanged: {
+					addListener: (callback) => {},
+					removeListener: (callback) => {},
 				}
 			}
 		}
-	}
 
-	await act(() => {
-		render(
-			<ChakraProvider>
-				<Provider store={store}>
-					<MyApp my_window={myWindow} />
-				</Provider>
-			</ChakraProvider>
-		)
-	})
-	await waitFor(() => {
+		await act(() => {
+			render(
+				<ChakraProvider>
+					<Provider store={store}>
+						<MyApp my_window={myWindow} />
+					</Provider>
+				</ChakraProvider>
+			)
+		})
+		await waitFor(() => {
 
+		})
+		mock.mockRestore()
+		mock2.mockRestore()
+		const winrateElem = screen.getByText(/winrate/i);
+		expect(winrateElem).toBeInTheDocument();
 	})
-	mock.mockRestore()
-	mock2.mockRestore()
-	// const linkElement = screen.getByText(/hello world/i);
-	// expect(linkElement).toBeInTheDocument();
 })
