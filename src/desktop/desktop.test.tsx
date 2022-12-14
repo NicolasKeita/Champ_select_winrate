bd20084b28044ea0be5d7fa60594a924c4573683
Nicolas KEITA <nicolas.keita@epitech.eu>
2022-12-14 17:48:47 +0100

    v1.1.2 add: third test : should display message when closing client

--- src/desktop/desktop.test.tsx
+++ src/desktop/desktop.test.tsx
@@ -10,16 +10,17 @@ import {kWindowNames} from '../consts'
 import {act, render, waitFor, screen} from '@testing-library/react'
 import {ChakraProvider} from '@chakra-ui/react'
 import {store} from '@utils/store/store'
-import * as fetchChampionsFromConfigJson from "@utils/fetchLocalConfigJson/fetchChampionsFromConfigJson"
-import fetchMock from "jest-fetch-mock";
+import * as fetchChampionsFromConfigJson
+	from '@utils/fetchLocalConfigJson/fetchChampionsFromConfigJson'
+import fetchMock from 'jest-fetch-mock'
 import allChamps from './../../__tests__/allChamps.json'
 import '@testing-library/jest-dom'
 
 const myWindow = new AppWindow(kWindowNames.desktop)
 
 describe('basic', () => {
-	const mock = jest.spyOn(fetchChampionsFromConfigJson, "fetchCSWgameVersion").mockResolvedValue("12.23.0")
-	const mock2 = jest.spyOn(fetchChampionsFromConfigJson, "fetchAllChampionsJson").mockResolvedValue(Object.values(allChamps))
+	const mock = jest.spyOn(fetchChampionsFromConfigJson, 'fetchCSWgameVersion').mockResolvedValue('12.23.0')
+	const mock2 = jest.spyOn(fetchChampionsFromConfigJson, 'fetchAllChampionsJson').mockResolvedValue(Object.values(allChamps))
 	global.overwolf = {
 		games: {
 			launchers: {
@@ -29,7 +30,8 @@ describe('basic', () => {
 					onInfoUpdates: {
 						addListener: (onInfoUpdates2Listener) => {}
 					},
-					getInfo: (numberr: any, callback: any) => {}
+					setRequiredFeatures: () => {},
+					getInfo: () => {}
 				},
 				getRunningLaunchersInfo: () => {},
 				//@ts-ignore
@@ -61,14 +63,14 @@ describe('basic', () => {
 		windows: {
 			onStateChanged: {
 				addListener: (callback) => {},
-				removeListener: (callback) => {},
+				removeListener: (callback) => {}
 			}
 		}
 	}
 	test('should display winrate in header', async () => {
 		fetchMock.enableMocks()
 		// @ts-ignore
-		fetch.mockResponseOnce(JSON.stringify(allChamps));
+		fetch.mockResponseOnce(JSON.stringify(allChamps))
 
 		await act(() => {
 			render(
@@ -84,24 +86,24 @@ describe('basic', () => {
 		})
 		mock.mockRestore()
 		mock2.mockRestore()
-		const winrateElem = screen.getByText(/winrate/i);
-		const FooterElem = screen.getByText(/League client is not open./i);
-		expect(winrateElem).toBeInTheDocument();
-		expect(FooterElem).toBeInTheDocument();
+		const winrateElem = screen.getByText(/winrate/i)
+		const FooterElem = screen.getByText(/League client is not open./i)
+		expect(winrateElem).toBeInTheDocument()
+		expect(FooterElem).toBeInTheDocument()
 	})
-
 	test('should display message when opening lol client', async () => {
 		fetchMock.enableMocks()
 		// @ts-ignore
-		fetch.mockResponseOnce(JSON.stringify(allChamps));
+		fetch.mockResponseOnce(JSON.stringify(allChamps))
 
 		function getRunningLaunchersInfo(callback) {
 			const clientsInfos = {
 				launchers: [] as unknown as {id: number}[]
 			}
-			clientsInfos.launchers.push( {id: 109021} )
+			clientsInfos.launchers.push({id: 109021})
 			callback(clientsInfos)
 		}
+
 		global.overwolf.games.launchers.getRunningLaunchersInfo = getRunningLaunchersInfo
 
 		await act(() => {
@@ -118,7 +120,46 @@ describe('basic', () => {
 		})
 		mock.mockRestore()
 		mock2.mockRestore()
-		const FooterElem = screen.getByText(/You are not in champ select./i);
-		expect(FooterElem).toBeInTheDocument();
+		const FooterElem = screen.getByText(/You are not in champ select./i)
+		expect(FooterElem).toBeInTheDocument()
 	})
+	test('should display message when closing lol client', async () => {
+		fetchMock.enableMocks()
+		// @ts-ignore
+		fetch.mockResponseOnce(JSON.stringify(allChamps))
+
+		function getRunningLaunchersInfo(callback) {
+			const clientsInfos = {
+				launchers: [] as unknown as {id: number}[]
+			}
+			clientsInfos.launchers.push({id: 109021})
+			callback(clientsInfos)
+		}
+
+		function onTerminatedAddListener(callback) {
+			setTimeout(callback, 1000)
+		}
+
+		// open LoL client
+		global.overwolf.games.launchers.getRunningLaunchersInfo = getRunningLaunchersInfo
+		// closes LoL client
+		global.overwolf.games.launchers.onTerminated.addListener = onTerminatedAddListener
+
+		await act(() => {
+			render(
+				<ChakraProvider>
+					<Provider store={store}>
+						<MyApp my_window={myWindow} />
+					</Provider>
+				</ChakraProvider>
+			)
+		})
+		mock.mockRestore()
+		mock2.mockRestore()
+		await waitFor(() => {
+			const FooterElem = screen.getByText(/League client is not open./i)
+			expect(FooterElem).toBeInTheDocument()
+		}, {timeout: 4000})
+	})
+
 })
